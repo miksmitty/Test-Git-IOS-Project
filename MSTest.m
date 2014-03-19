5e6cd9a388349ed196e6a3a844e96ca03276bb79
= <mikesmithster@gmail.com>
2014-03-19 15:45:44 +0000

    added a test class

---
+++ MSTest.m
@@ -0,0 +1,13 @@
+//
+//  MSTest.m
+//  GitTest
+//
+//  Created by Mike Smith on 19/03/2014.
+//  Copyright (c) 2014 self.edu.com. All rights reserved.
+//
+
+#import "MSTest.h"
+
+@implementation MSTest
+
+@end
